0a2d0784185170e5c42a69bfe69155ee44889861
natanael nogueira <natanaelnogueira019@gmail.com>
2022-07-05 20:07:17 -0300

    alteracoes globai

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,23 @@
 //aqui serve para usar alteracoes globais, ou melhor importar arquivos com alterações globais
 
-import '..//src/style.css'
+import Head from 'next/head'
+
+import Title from '..///src/compenentes/title/Title'
+import Subtitle from '..//src/compenentes/subtitle/subtitle'
 function App({Component, pageProps}){
 
-return <Component {...pageProps}/>
-
+return (
+<>
+  <Head>Titulo Padrao</Head>
+  <div>
+    <Component {...pageProps}/>
+    <Title> Um título</Title>
+    <Subtitle text ='Um subtitulo'/>
+    <Title>Outro Titulo</Title>
+    <Subtitle text = 'Outro Subtitulo'/>
+  </div>
+</>
+)
 }
 
 export default App
