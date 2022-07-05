a4b0fdce83c878137350f8c46c43116d2ac4a802
natanael nogueira <natanaelnogueira019@gmail.com>
2022-07-05 00:55:10 -0300

    novos componentes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,12 @@
+import Title from '../src/compenentes/title/Title'
+
+
 function HomePage(){
   return(
     <div>
+      <Title />
+      <Title />
+      <Title />
       Hello World, NextJs + reactJs
     </div>
   )
---
+++ src/compenentes/title/Title.jsx
@@ -0,0 +1,9 @@
+
+
+function Title (){
+  return(
+    <h1>Meu Titulo</h1>
+  )
+}
+
+export default Title
