977188aa7fb43acace7a9caa1f04b2695f5c5be3
natanael nogueira <natanaelnogueira019@gmail.com>
2022-07-08 23:00:29 -0300

    main

--- src/compenentes/header/header.jsx
+++ src/compenentes/header/header.jsx
@@ -2,7 +2,13 @@ import styles from './header.module.css'
 function Header ({children}){
   return(
     <header 
-    className ={styles.header} >
+    className ={styles.header} 
+    style ={{ 
+    background: `url('https://sejadev-web-app-public-files.s3.amazonaws.com/courses/coffee1.jpg')`,
+    backgroundPosition: `center`,
+    backgroundRepeat: `no-repeat`
+   }}
+    >
      <div className = {styles.headerContent}>
       {children}
       </div>
